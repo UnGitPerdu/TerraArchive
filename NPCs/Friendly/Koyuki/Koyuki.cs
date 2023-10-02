928ab1999ef2892b32510afbc20576aa54859b08
UnGitPerdu <unjoueruperdu@gmail.com>
2023-10-02 18:23:35 +0200

    Koyuki & Noa folders

---
+++ NPCs/Friendly/Koyuki/Koyuki.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerraArchive.NPCs.Friendly.Koyuki
+{
+    internal class Koyuki
+    {
+    }
+}
