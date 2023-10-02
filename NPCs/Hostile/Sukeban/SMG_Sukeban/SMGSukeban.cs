0b99002581f93a2ec0b2fa92623ebfd3a001ded9
UnGitPerdu <unjoueruperdu@gmail.com>
2023-10-02 19:06:26 +0200

    Sukeban NPC files insertion

---
+++ NPCs/Hostile/Sukeban/SMG_Sukeban/SMGSukeban.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerraArchive.NPCs.Hostile.Sukeban.SMG_Sukeban
+{
+    internal class SMGSukeban
+    {
+    }
+}
