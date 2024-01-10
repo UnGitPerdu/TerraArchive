e3446d19ac37ce89cc5b6f416bf4b2243d35484d
Einzeilhandery <138909876+Einzeilhandery@users.noreply.github.com>
2024-01-11 00:15:57 +0700

    Add files via upload

---
+++ NPCs/Hostile/Sukeban/MG_Sukeban/NPC_MGSukeban.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerraArchive.NPCs.Hostile.Sukeban.MG_Sukeban
+{
+    internal class NPC_MGSukeban
+    {
+    }
+}
---
+++ NPCs/Hostile/Sukeban/SR_Sukeban/SRSukeban.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.IO;
+using Terraria;
+using Terraria.GameContent.Bestiary;
+using Terraria.GameContent.UI;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using Terraria.ModLoader.Utilities;
+
+namespace TerraArchive.NPCs.Hostile.Sukeban.SR_Sukeban
+{
+    internal class NPC_SRSukeban : ModNPC
+    {
+        public int StolenItems = 0;
+
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[Type] = Main.npcFrameCount[NPCID.PirateDeadeye];
+
+            NPCID.Sets.NPCBestiaryDrawModifiers value = new NPCID.Sets.NPCBestiaryDrawModifiers()
+            {
+                // Influences how the NPC looks in the Bestiary
+                Velocity = 1f // Draws the NPC in the bestiary as if its walking +1 tiles in the x direction
+            };
+            NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, value);
+        }
+
+        public override void SetDefaults()
+        {
+            NPC.width = 18;
+            NPC.height = 40;
+            NPC.damage = 14;
+            NPC.defense = 6;
+            NPC.lifeMax = 200;
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath2;
+            NPC.value = 60f;
+            NPC.knockBackResist = 0.5f;
+
+            AIType = NPCID.PirateDeadeye; // Use vanilla zombie's type when executing AI code. (This also means it will try to despawn during daytime)
+            AnimationType = NPCID.PirateDeadeye; // Use vanilla zombie's type when executing animation code. Important to also match Main.npcFrameCount[NPC.type] in SetStaticDefaults.
+            //Banner = Item.NPCtoBanner(NPCID.Zombie); // Makes this NPC get affected by the normal zombie banner.
+            //BannerItem = Item.BannerToItem(Banner); // Makes kills of this NPC go towards dropping the banner it's associated with.
+            //SpawnModBiomes = new int[] { ModContent.GetInstance<ExampleSurfaceBiome>().Type }; // Associates this NPC with the ExampleSurfaceBiome in Bestiary
+        }
+
+        public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+        {
+            // We can use AddRange instead of calling Add multiple times in order to add multiple items at once
+            bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[] {
+				// Sets the spawning conditions of this NPC that is listed in the bestiary.
+				BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Times.DayTime,
+
+				// Sets the description of this NPC that is listed in the bestiary.
+				new FlavorTextBestiaryInfoElement("Sniper Rifle Sukeban"),
+            });
+        }
+
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            return SpawnCondition.OverworldDaySlime.Chance * .5f; // Spawn with 1/2 the chance of a regular slime.
+        }
+
+    }
+}
