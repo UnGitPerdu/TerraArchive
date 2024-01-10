3734a7f02091e70d8a58b836145ceffa264eff47
Einzeilhandery <138909876+Einzeilhandery@users.noreply.github.com>
2024-01-11 01:05:33 +0700

    Update Player_AruLegging.cs

--- NPCs/School/Gehenna/Problem_Solver_68/Player/Aru/Player_AruLegging.cs
+++ NPCs/School/Gehenna/Problem_Solver_68/Player/Aru/Player_AruLegging.cs
@@ -3,7 +3,7 @@ using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
 
-namespace TerraArchive.NPCs.School.Gehenna.Problem_Solver_68.Player..Aru
+namespace TerraArchive.NPCs.School.Gehenna.Problem_Solver_68.Player.Aru
 {
 	// The AutoloadEquip attribute automatically attaches an equip texture to this item.
 	// Providing the EquipType.Legs value here will result in TML expecting a X_Legs.png file to be placed next to the item's main texture.
