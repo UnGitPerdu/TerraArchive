7300cf72787881106a089bd4333d9d8d8e8df0cc
Einzeilhandery <138909876+Einzeilhandery@users.noreply.github.com>
2024-01-11 01:01:59 +0700

    Update Player_AruHead.cs

--- NPCs/School/Gehenna/Problem_Solver_68/Player/Aru/Player_AruHead.cs
+++ NPCs/School/Gehenna/Problem_Solver_68/Player/Aru/Player_AruHead.cs
@@ -4,7 +4,7 @@ using Terraria.Localization;
 using Terraria.ModLoader;
 using testsensei.Aru;
 
-namespace testsensei.Aru
+namespace NPCs.School.Gehenna.Problem_Solver_68.Player.Aru
 {
 	// The AutoloadEquip attribute automatically attaches an equip texture to this item.
 	// Providing the EquipType.Head value here will result in TML expecting a X_Head.png file to be placed next to the item's main texture.
