8df07b75aa66f92f348a1e6ea58d0d2bc397403a
Einzeilhandery <138909876+Einzeilhandery@users.noreply.github.com>
2024-01-11 00:06:28 +0700

    Add files via upload

---
+++ Items/Accessories/tier_1_items/T1_PlainBaseballCap.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TerraArchive.Items.Accessories.tier_1_items
+{
+    [AutoloadEquip(EquipType.Head)]
+    public class T1_PlainBaseballCap : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 16;
+            Item.height = 16;
+
+            // Common values for every boss mask
+            Item.rare = ItemRarityID.Blue;
+            Item.value = Item.sellPrice(silver: 75);
+            Item.vanity = true;
+            Item.maxStack = 1;
+        }
+    }
+}
