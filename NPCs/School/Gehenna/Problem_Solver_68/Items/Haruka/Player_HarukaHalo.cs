12390b74a59024e23e04c1b64a6423cac12a9ff3
Einzeilhandery <138909876+Einzeilhandery@users.noreply.github.com>
2024-02-25 22:23:31 +0700

    Blirtz

---
+++ NPCs/School/Gehenna/Problem_Solver_68/Items/Haruka/Player_HarukaHalo.cs
@@ -0,0 +1,26 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace testsensei.Haruka
+{
+    [AutoloadEquip(EquipType.Beard)]
+    public class Player_HarukaHalo : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            ArmorIDs.Beard.Sets.UseHairColor[Item.beardSlot] = false;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 30;
+            Item.height = 100;
+            Item.maxStack = 1;
+            Item.value = Item.sellPrice(0, 1);
+            Item.accessory = true;
+            Item.vanity = true;
+        }
+    }
+
+}
